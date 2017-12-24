0eba4f6fbf1269083736972da1f973afe325ba7b
maayananafi <34560265+maayananafi@users.noreply.github.com>
2017-12-24 12:44:08 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache the value of the matrix and its inverse, and then returns the inverse if it cached and computes it if it is not cached.
 
-## Write a short comment describing this function
+## This function sets and gets the value of the matrix, and sets and gets the value of the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im <- NULL
+  setMatrix <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  getMatrix <- function() x
+  setinverse <- function(inverse) im <<- inverse
+  getinverse <- function () im
+  list(setMatrix=setMatrix,
+       getMatrix=getMatrix,
+       setinverse=setinverse,
+       getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse matrix. It uses ifelse to first determine if there is a cached inverse matrix. If there is no cached inverse matrix (that is, if im is null), it computes the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  im <- x$getinverse()
+  if(!is.null(im)) {
+    message("Printing cached inverse matrix")
+    return(im)
+  }
+  data <- x$getMatrix()
+  i <- solve(data,...)
+  x$setinverse(i)
+  i
 }
+
+## Testing
+
+my_matrix <- matrix(1:4,2)
+solve(my_matrix)
+my_matrix1 <- makeCacheMatrix(my_matrix)
+cacheSolve(my_matrix1)
+identical(solve(my_matrix),cacheSolve(my_matrix1))
